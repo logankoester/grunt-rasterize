be80e92049395512122254eb47d5308be6f77749
Logan Koester <logan@logankoester.com>
2014-02-01 05:37:13 -0500

    Updates grunt-contrib-coffee dependency to

--- tasks/rasterize.js
+++ tasks/rasterize.js
@@ -9,18 +9,19 @@
 
   module.exports = function(grunt) {
     return grunt.registerMultiTask('rasterize', 'Convert a vector graphic into one or more raster images', function() {
-      var done, rasterize,
-        _this = this;
-      rasterize = function(out, done) {
-        var ink;
-        grunt.log.write(" -> " + out.path + " ");
-        ink = new Inkscape(['--export-png', "--export-width=" + out.width]).on('end', function() {
-          return done();
-        });
-        out = fs.createWriteStream(out.path);
-        fs.createReadStream(_this.data.vector).pipe(ink).pipe(out);
-        return grunt.log.ok();
-      };
+      var done, rasterize;
+      rasterize = (function(_this) {
+        return function(out, done) {
+          var ink;
+          grunt.log.write(" -> " + out.path + " ");
+          ink = new Inkscape(['--export-png', "--export-width=" + out.width]).on('end', function() {
+            return done();
+          });
+          out = fs.createWriteStream(out.path);
+          fs.createReadStream(_this.data.vector).pipe(ink).pipe(out);
+          return grunt.log.ok();
+        };
+      })(this);
       grunt.log.subhead("Rasterizing " + this.data.vector);
       done = this.async();
       return async.eachSeries(this.data.raster, rasterize, function(err) {
